2451cff61aebb8c75786317727d28f5ea72c86f0
johnathikalam <johnathikalam3@gmail.com>
2025-07-04 18:04:42 +0530

    solved alpha hill pattern

---
+++ basics/patterns/alpha_hill_pattern/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void pattern(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n-i; j++){
+            cout<< " ";
+        }
+        for (int j = 0; j <= i; j++){
+            cout<< char('A'+j);
+        }
+        for (int j = 1; j <= i; j++){
+            cout<< char('A' + i-j);
+        }
+        cout<< endl;
+    }
+}
+
+int main(){
+    pattern(6);
+    return 0;
+}
