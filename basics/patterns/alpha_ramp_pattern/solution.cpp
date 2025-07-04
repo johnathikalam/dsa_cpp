eebe9a3aa03aff48e477cd63594874296be51475
johnathikalam <johnathikalam3@gmail.com>
2025-07-04 17:45:00 +0530

    solved alpha ramp pattern

---
+++ basics/patterns/alpha_ramp_pattern/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void pattern(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout<< char('A'+i)<< " ";
+        }
+        cout<< endl;
+    }
+}
+
+int main(){
+    pattern(6);
+    return 0;
+}
