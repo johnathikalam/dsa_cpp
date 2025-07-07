54923a9efdf7ceb5a98dfa43b2e01e2a505e6344
johnathikalam <johnathikalam3@gmail.com>
2025-07-07 11:50:37 +0530

    solved symmetric butterfly pattern

---
+++ basics/patterns/symmetric_buttterfly_pattern/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void pattern(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < i+1; j++){
+            cout<< "*";
+        }
+        for (int j = 0; j < 2*(n-i-1); j++){
+            cout<< " ";
+        }
+        for (int j = 0; j < i+1; j++){
+            cout<< "*";
+        }
+        cout<< endl;
+    }
+
+    for (int i =0; i < n; i++){
+        for (int j = 0; j < n-i-1; j++){
+            cout<< "*";
+        }
+        for (int j = 0; j < 2*(i+1); j++){
+            cout<< " ";
+        }
+        for (int j = 0; j < n-i-1; j++){
+            cout<< "*";
+        }
+        cout<< endl;
+    }
+}
+
+int main(){
+    pattern(6);
+    return 0;
+}
